e426502e114725761d06d5a50522976942c2c543
0Villian0 <3133348848@qq.com>
2018-08-10 20:53:24 +0800

    pvp model snake

--- PvP_snake.py
+++ PvP_snake.py
@@ -21,7 +21,7 @@ def initialize_windows():
     curses.cbreak()                                                                                                     # 开启cbreak模式，键盘输入立即执行，而不是缓冲输入模式那样等待回车后才执行
     curses.noecho()                                                                                                     # 关闭键盘输入屏幕回显
     w.keypad(True)                                                                                                      # 开启特殊键位输入处理功能 如：上下左右键等
-    w.timeout(1)                                                                                                        # 设置读取键盘输入等待时间 单位：毫秒
+    w.timeout(1000)                                                                                                        # 设置读取键盘输入等待时间 单位：毫秒
     H, W = w.getmaxyx()                                                                                                 # 获取w窗口的长宽
     # H, W = 11, 11                                                                                                     # 把窗口设为固定值 调试用
     food = [[int(H / 2), int(W / 2)]]                                                                                   # initialize the position of food
@@ -42,22 +42,129 @@ def recover_windows():                                                          
     curses.endwin()
 
 
-def player():                                                                                                           # 玩家操控贪吃蛇
-    global key
+# def player():                                                                                                           # 玩家操控贪吃蛇
+#     global key
+#     while True:
+#         next_key = w.getch()                                                                                            # 获取下一个方向键
+#         if next_key == 27:                                                                                              # 按下Esc鍵或者發生error則恢復窗口後退出
+#             recover_windows()
+#             print('QUIT GAME! \nScore:' + str(score))
+#             return
+#
+#         if next_key == curses.KEY_RIGHT and key != curses.KEY_LEFT \
+#                 or next_key == curses.KEY_LEFT and key != curses.KEY_RIGHT \
+#                 or next_key == curses.KEY_UP and key != curses.KEY_DOWN \
+#                 or next_key == curses.KEY_DOWN and key != curses.KEY_UP:
+#             key = next_key                                                                                              # 获取有效的运动方向键
+#
+#         temp = snake[0]                                                                                                 # 更新蛇头位置
+#         if key == curses.KEY_UP:
+#             snake.insert(0, [temp[0] - 1, temp[1]])
+#         if key == curses.KEY_DOWN:
+#             snake.insert(0, [temp[0] + 1, temp[1]])
+#         if key == curses.KEY_RIGHT:
+#             snake.insert(0, [temp[0], temp[1] + 1])
+#         if key == curses.KEY_LEFT:
+#             snake.insert(0, [temp[0], temp[1] - 1])
+#
+#         if snake[0] in snake[1:] or snake[0][0] in (0, H) or snake[0][1] in (0, W):                                     # 判断蛇是否死亡,是则恢复默认窗口退出
+#             # recover_windows()
+#             # print('Your Snake is death! \nScore:' + str(score))
+#             return
+#
+#         eaten()
+#         w.addch(snake[0][0], snake[0][1], 'X', curses.color_pair(1))
+#
+#
+# def player_1():                                                                                                         # 玩家操控贪吃蛇
+#     global key_1
+#     while True:
+#         next_key = w.getch()                                                                                            # 获取下一个方向键
+#         if next_key == 27:                                                                                              # 按下Esc鍵或者發生error則恢復窗口後退出
+#             recover_windows()
+#             print('QUIT GAME! \nScore:' + str(score_1))
+#             return
+#
+#         if next_key == 100 and key_1 != 97 \
+#                 or next_key == 97 and key_1 != 100 \
+#                 or next_key == 119 and key_1 != 115 \
+#                 or next_key == 115 and key_1 != 119:
+#             key_1 = next_key                                                                                            # 获取有效的运动方向键
+#
+#         temp = snake_1[0]                                                                                               # 更新蛇头位置
+#         if key_1 == 119:
+#             snake_1.insert(0, [temp[0] - 1, temp[1]])
+#         if key_1 == 115:
+#             snake_1.insert(0, [temp[0] + 1, temp[1]])
+#         if key_1 == 100:
+#             snake_1.insert(0, [temp[0], temp[1] + 1])
+#         if key_1 == 97:
+#             snake_1.insert(0, [temp[0], temp[1] - 1])
+#
+#         if snake_1[0] in snake_1[1:] or snake_1[0][0] in (0, H) or snake_1[0][1] in (0, W):                             # 判断蛇是否死亡,是则恢复默认窗口退出
+#             # recover_windows()
+#             # print('Your Snake is death! \nScore:' + str(score_1))
+#             return
+#
+#         eaten(1)
+#         w.addch(snake_1[0][0], snake_1[0][1], 'X', curses.color_pair(3))
+#
+#
+# def eaten(num=0):                                                                                                       # 检查食物状态
+#     global score, score_1
+#     if num == 0:
+#         if snake[0] == food[0]:                                                                                         # 判断是否吃掉食物
+#             temp = food.pop()
+#             w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
+#             if len(snake) < (H - 1) * (W - 1) - len(snake_1):                                                           # 判断是否走完
+#                 while temp in snake or temp in snake_1:                                                                 # 隨機產生一個食物
+#                     temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
+#                 food.insert(0, temp)
+#                 w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
+#             score += 1                                                                                                  # 記錄分數
+#         else:
+#             tail = snake.pop()                                                                                          # 沒有吃到食物需要把尾巴去掉
+#             w.addch(tail[0], tail[1], ' ')
+#     elif num == 1:
+#         if snake_1[0] == food[0]:                                                                                       # 判断是否吃掉食物
+#             temp = food.pop()
+#             w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
+#             if len(snake_1) < (H - 1) * (W - 1) - len(snake):                                                           # 判断是否走完
+#                 while temp in snake_1 or temp in snake:                                                                 # 隨機產生一個食物
+#                     temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
+#                 food.insert(0, temp)
+#                 w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
+#             score_1 += 1                                                                                                # 記錄分數
+#         else:
+#             tail = snake_1.pop()                                                                                        # 沒有吃到食物需要把尾巴去掉
+#             w.addch(tail[0], tail[1], ' ')
+
+
+def played():
+    global key, key_1
+    next_key = None
     while True:
-        next_key = w.getch()                                                                                            # 获取下一个方向键
-        if next_key == 27:                                                                                              # 按下Esc鍵或者發生error則恢復窗口後退出
-            recover_windows()
-            print('QUIT GAME! \nScore:' + str(score))
-            return
+        if K:
+            lock.acquire()
+            next_key = K.pop(0)
+            lock.release()
+            if next_key == 27:
+                recover_windows()
+                print('QUIT GAME! \nPlayer1 Score: %d\nPlayer2 Score: %d' % (score, score_1))
+                return
 
         if next_key == curses.KEY_RIGHT and key != curses.KEY_LEFT \
                 or next_key == curses.KEY_LEFT and key != curses.KEY_RIGHT \
                 or next_key == curses.KEY_UP and key != curses.KEY_DOWN \
                 or next_key == curses.KEY_DOWN and key != curses.KEY_UP:
-            key = next_key                                                                                              # 获取有效的运动方向键
+            key = next_key
+        elif next_key == 100 and key_1 != 97 \
+                or next_key == 97 and key_1 != 100 \
+                or next_key == 119 and key_1 != 115 \
+                or next_key == 115 and key_1 != 119:
+            key_1 = next_key
 
-        temp = snake[0]                                                                                                 # 更新蛇头位置
+        temp = snake[0]  # 更新蛇头位置
         if key == curses.KEY_UP:
             snake.insert(0, [temp[0] - 1, temp[1]])
         if key == curses.KEY_DOWN:
@@ -67,31 +174,7 @@ def player():                                                                   
         if key == curses.KEY_LEFT:
             snake.insert(0, [temp[0], temp[1] - 1])
 
-        if snake[0] in snake[1:] or snake[0][0] in (0, H) or snake[0][1] in (0, W):                                     # 判断蛇是否死亡,是则恢复默认窗口退出
-            # recover_windows()
-            # print('Your Snake is death! \nScore:' + str(score))
-            return
-
-        eaten()
-        w.addch(snake[0][0], snake[0][1], 'X', curses.color_pair(1))
-
-
-def player_1():                                                                                                         # 玩家操控贪吃蛇
-    global key_1
-    while True:
-        next_key = w.getch()                                                                                            # 获取下一个方向键
-        if next_key == 27:                                                                                              # 按下Esc鍵或者發生error則恢復窗口後退出
-            recover_windows()
-            print('QUIT GAME! \nScore:' + str(score_1))
-            return
-
-        if next_key == 100 and key_1 != 97 \
-                or next_key == 97 and key_1 != 100 \
-                or next_key == 119 and key_1 != 115 \
-                or next_key == 115 and key_1 != 119:
-            key_1 = next_key                                                                                            # 获取有效的运动方向键
-
-        temp = snake_1[0]                                                                                               # 更新蛇头位置
+        temp = snake_1[0]  # 更新蛇头位置
         if key_1 == 119:
             snake_1.insert(0, [temp[0] - 1, temp[1]])
         if key_1 == 115:
@@ -101,57 +184,69 @@ def player_1():                                                                 
         if key_1 == 97:
             snake_1.insert(0, [temp[0], temp[1] - 1])
 
-        if snake_1[0] in snake_1[1:] or snake_1[0][0] in (0, H) or snake_1[0][1] in (0, W):                             # 判断蛇是否死亡,是则恢复默认窗口退出
-            # recover_windows()
-            # print('Your Snake is death! \nScore:' + str(score_1))
+        if snake[0] in snake[1:] or snake[0][0] in (0, H) or snake[0][1] in (0, W) \
+                or snake_1[0] in snake_1[1:] or snake_1[0][0] in (0, H) or snake_1[0][1] in (0, W):
             return
 
-        eaten(1)
+        eaten()
+        w.addch(snake[0][0], snake[0][1], 'X', curses.color_pair(1))
         w.addch(snake_1[0][0], snake_1[0][1], 'X', curses.color_pair(3))
+        w.getch()
 
 
-def eaten(num=0):                                                                                                       # 检查食物状态
+def eaten():                                                                                                       # 检查食物状态
     global score, score_1
-    if num == 0:
-        if snake[0] == food[0]:                                                                                         # 判断是否吃掉食物
-            temp = food.pop()
-            w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
-            if len(snake) < (H - 1) * (W - 1) - len(snake_1):                                                           # 判断是否走完
-                while temp in snake or temp in snake_1:                                                                 # 隨機產生一個食物
-                    temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
-                food.insert(0, temp)
-                w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
-            score += 1                                                                                                  # 記錄分數
-        else:
-            tail = snake.pop()                                                                                          # 沒有吃到食物需要把尾巴去掉
-            w.addch(tail[0], tail[1], ' ')
-    elif num == 1:
-        if snake_1[0] == food[0]:                                                                                       # 判断是否吃掉食物
-            temp = food.pop()
-            w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
-            if len(snake_1) < (H - 1) * (W - 1) - len(snake):                                                           # 判断是否走完
-                while temp in snake_1 or temp in snake:                                                                 # 隨機產生一個食物
-                    temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
-                food.insert(0, temp)
-                w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
-            score_1 += 1                                                                                                # 記錄分數
-        else:
-            tail = snake_1.pop()                                                                                        # 沒有吃到食物需要把尾巴去掉
-            w.addch(tail[0], tail[1], ' ')
+    if snake[0] == food[0]:                                                                                         # 判断是否吃掉食物
+        temp = food.pop()
+        w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
+        if len(snake) < (H - 1) * (W - 1) - len(snake_1):                                                           # 判断是否走完
+            while temp in snake or temp in snake_1:                                                                 # 隨機產生一個食物
+                temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
+            food.insert(0, temp)
+            w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
+        score += 1                                                                                                  # 記錄分數
+    else:
+        tail = snake.pop()                                                                                          # 沒有吃到食物需要把尾巴去掉
+        w.addch(tail[0], tail[1], ' ')
+
+    if snake_1[0] == food[0]:                                                                                       # 判断是否吃掉食物
+        temp = food.pop()
+        w.addch(temp[0], temp[1], ' ')                                                                              # 把吃掉的食物打印為空
+        if len(snake_1) < (H - 1) * (W - 1) - len(snake):                                                           # 判断是否走完
+            while temp in snake_1 or temp in snake:                                                                 # 隨機產生一個食物
+                temp = [random.randint(1, H - 1), random.randint(1, W - 1)]
+            food.insert(0, temp)
+            w.addch(temp[0], temp[1], '$', curses.color_pair(2))                                                    # 打印顯示新食物
+        score_1 += 1                                                                                                # 記錄分數
+    else:
+        tail = snake_1.pop()                                                                                        # 沒有吃到食物需要把尾巴去掉
+        w.addch(tail[0], tail[1], ' ')
+
+
+def get_key():
+    while True:
+        getkey = w.getch()
+        if getkey in (27, 97, 100, 115, 119, 258, 259, 260, 261):
+            lock.acquire()
+            K.append(getkey)
+            lock.release()
+        if getkey == 27:
+            return
 
 
 def main():
     try:                                                                                                                # 拦截程序内的错误
         initialize_windows()                                                                                            # 初始化窗口
-        p = threading.Thread(target=player)
-        threads.append(p)
-        p = threading.Thread(target=player_1)
-        threads.append(p)
-        for i in threads:
+        p = threading.Thread(target=played)
+        lists.append(p)
+        p = threading.Thread(target=get_key)
+        lists.append(p)
+        for i in lists:
             i.start()
-        for i in threads:
+        for i in lists:
             i.join()
         recover_windows()                                                                                               # 恢复默认窗口
+        print(K)
 
     except Exception as E:
         recover_windows()
@@ -161,5 +256,6 @@ def main():
 
 
 if __name__ == '__main__':
-    w, H, W, food, snake, snake_1, score, score_1, key, key_1, threads = object, 0, 0, [], [], [], 0, 0, 0, 0, []       # 定义全局变量和对象
+    w, H, W, food, snake, snake_1, score, score_1, key, key_1, lists, K = object, 0, 0, [], [], [], 0, 0, 0, 0, [], []       # 定义全局变量和对象
+    lock = threading.Lock
     main()
